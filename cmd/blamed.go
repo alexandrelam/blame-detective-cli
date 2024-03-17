e72615a0019a05ce9290296094775c21fa55cc92
Alexandre Lam <alexandrelam@outlook.com>
2024-03-17 15:32:38 +0100

    elapsed time in second

--- cmd/blamed.go
+++ cmd/blamed.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 
 	"github.com/alexandrelam/blame-detective-cli/pkg"
 )
 
 func main() {
 	fmt.Println("Welcome to Blame Detective CLI!")
+	startTime := time.Now()
 
 	// We parse the flags
 	var parsedFlags = pkg.ParseFlags()
@@ -24,7 +26,12 @@ func main() {
 	// Open vscode with the folder
 	exec.Command("code", tmpDir).Run()
 
+	// Size of the folder
 	output, _ := exec.Command("du", "-sh", tmpDir).Output()
 	fmt.Println("\nFolder size: " + string(output))
+
+	// Measure time taken in seconds
+	elapsedTime := time.Since(startTime).Seconds()
+	fmt.Printf("Execution time: %.2f seconds\n", elapsedTime)
 	fmt.Println("Done!")
 }
